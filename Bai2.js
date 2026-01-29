582a1537372967f450874afb7b59e4de8b260fab
TruongVu2511 <truongvun781@gmail.com>
2026-01-29 21:41:19 +0700

    Fix prompt input conversion for number of books

--- Bai2.js
+++ Bai2.js
@@ -1,8 +1,9 @@
-let numberBook = prompt ("Nhập số lượng sách:");
+let numberBook = +prompt ("Nhập số lượng sách:");
 if(numberBook < 10){
     console.log(`Thư viện có ít sách`);
 }else if(numberBook >= 10 && numberBook <= 20){
     console.log(`Thư viện có số lượng sách vừa đủ`);
 }else{
     console.log(`Thư viện có nhiều sách`);
+
 }
