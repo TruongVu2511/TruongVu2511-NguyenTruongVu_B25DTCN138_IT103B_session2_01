9430556465f401da7f849850bde3c7d719548f08
TruongVu2511 <truongvun781@gmail.com>
2026-01-29 21:42:10 +0700

    Fix formatting and add newline at end of file

--- Bai5.js
+++ Bai5.js
@@ -1,6 +1,6 @@
 let nameBook = prompt ("Nhập tên sách:");
 let statusBook = prompt ("Nhập tình trạng sách(có sẵn hay đã mượn) :");
-let yearBook = prompt ("Nhập năm xuất bản:");
+let yearBook = +prompt ("Nhập năm xuất bản:");
 if(statusBook === "có sẵn" && yearBook >= 2021 && yearBook <= 2026){
     console.log("Sách này mới và có sẵn để mượn");
 }else if(statusBook === "đã mượn" && yearBook >= 2016 && yearBook <= 2026){
@@ -9,4 +9,5 @@ if(statusBook === "có sẵn" && yearBook >= 2021 && yearBook <= 2026){
     console.log("Sách này đã mượn và khá cũ");
 }else if(statusBook === "có sẵn" && yearBook < 2021){
     console.log("Sách này có sẵn nhưng đã lâu năm");
+
 }
